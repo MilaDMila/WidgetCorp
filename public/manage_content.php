118bfbec50f78dd5ea1c6182077c056e7402b3ab
MilaDMila <milayu@hotmail.com>
2023-12-18 03:09:17 +0100

    moving on

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,36 +1,46 @@
-
 <?php include("../includes/functions.php"); ?>
 
 <!-- Upit ka bazi -->
 
-<?php
-    $conection = mysqli_connect("localhost", "root", "", "widget_corp") or die("Error");
-    $baza = "SELECT * FROM subjects ";
-    $upit = mysqli_query($conection, $baza);
-    $result = mysqli_fetch_all($upit, MYSQLI_ASSOC);
-    // if (!$result) {
-    //     die("Database query failed.");
-    // }
-    ?>
-
 <?php include("../includes/layouts/header.php"); ?>
 
 <div class="main">
-        <div id="navigation">
-          <ul class="subjects">
-<!-- Koriscenje vracenih podataka -->
-            <?php 
-            foreach($result as $subject){
-            ?>
-            <li><?php echo $subject["menu_name"]; ?></li>
-            <?php 
-            }
-            ?>
-          </ul>
-        </div>
-        <div id="page">
-            <h2>Manage Content</h2>
-        </div>
+  <div id="navigation">
+    <ul class="subjects">
+      <?php
+      $conection = mysqli_connect("localhost", "root", "", "widget_corp") or die("Error");
+      $baza = "SELECT * FROM subjects ";
+      $upit = mysqli_query($conection, $baza);
+      $subject_set = mysqli_fetch_all($upit, MYSQLI_ASSOC);
+      ?>
+      <!-- Koriscenje vracenih podataka -->
+      <?php
+      foreach ($subject_set as $subject) {
+      ?>
+        <li><?php echo $subject["menu_name"]; ?></li>
+      <?php
+      }
+      ?>
+    </ul>
+    <ul class="pages">
+      <?php
+      $conection = mysqli_connect("localhost", "root", "", "widget_corp") or die("Error");
+      $baza = "SELECT * FROM pages ";
+      $upit = mysqli_query($conection, $baza);
+      $page_set = mysqli_fetch_all($upit, MYSQLI_ASSOC);
+      ?>
+      <?php
+      foreach ($page_set as $page) {
+      ?>
+        <li><?php echo $page["menu_name"]; ?></li>
+      <?php
+      }
+      ?>
+    </ul>
+  </div>
+  <div id="page">
+    <h2>Manage Content</h2>
+  </div>
 </div>
 <!-- Release returned data -->
 <?php include("../includes/layouts/footer.php"); ?>
